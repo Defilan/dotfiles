df3916a8bc97617f57e0904a99fac6dd735e673c
Christopher Maher <chris@mahercode.io>
2022-04-17 15:03:44 -0700

    Bash script changes

--- install.sh
+++ install.sh
@@ -10,9 +10,22 @@ git submodule update --init --recursive
 if [ "$(uname)" == "Darwin" ]; then
     echo "Running on OSX"
 
-    echo "Installing Homebrew..."
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
+    echo "Checking/Installing Homebrew..."
+    install=0
+    if [[ $(uname -m) == 'arm64' ]]; then
+        echo "M1"
+        if [ ! -d "/opt/homebrew" ]; then
+            install=1
+        fi
+    else
+        echo "Intel"
+        if [ ! -d "/usr/local" ]; then
+            install=1
+        fi
+    fi
+    if [ "$install" == "1" ]; then
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    fi
     echo "Install Ansible..."
     brew install ansible
     echo "Configuring Mac..."
--- install/brew.sh
+++ install/brew.sh
@@ -5,47 +5,5 @@ if test ! $(which brew); then
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-echo "Installing homebrew packages..."
-
-# cli tools
-brew install wget
-#tools for chef dev
-brew install packer
-brew cask install vagrant
-brew tap homebrew/cask-fonts
-brew tap habitat-sh/habitat
-brew install hab
-brew install git-extras
-brew install dotnet-sdk
-brew install mono
-brew install kubernetes-cli
-
-# development tools
-brew install git
-brew cask install visual-studio-code
-
-# terminals cause why not!
-brew cask install iterm2
-brew install hub
-brew install macvim --override-system-vim
-brew install tmux
-brew install zsh
-brew install highlight
-brew install nvm
-brew install markdown
-brew install node
-brew install zsh-syntax-highlighting
-brew install zsh-autosuggestions
-brew install nmap 
-echo "Installing desktop application..."
-
-brew cask install spotify
-brew cask install slack
-brew cask install virtualbox
-brew cask install iterm2
-
-echo "Installing neovim..."
-# install neovim
-brew install neovim
-
+brew install ansible
 exit 0
