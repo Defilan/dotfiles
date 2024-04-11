cfe6aa68736d9d4c886f73c63be91cec4da5b2b8
Christopher P. Maher <chris@mahercode.io>
2024-04-10 21:59:33 -0700

    Adding different build types

--- install.sh
+++ install.sh
@@ -1,10 +1,35 @@
 #!bin/bash
 
+case $config_choice in
+  1)
+    config="bare_bones"
+    ;;
+  2)
+    config="developer"
+    ;;
+  3)
+    config="home"
+    ;;
+  *)
+    echo "Invalid choice, defaulting to Bare Bones"
+    config="bare_bones"
+    ;;
+esac
+
+export CONFIG=$config
+
 echo "Installing dotfiles"
 source install/link.sh
 
 echo "Initializing submodule(s)"
 git submodule update --init --recursive
+
+echo "Please choose a configuration:"
+echo "1) Lightweight (Essentials only)"
+echo "2) Developer/Engineering Config"
+echo "3) Productivity/Full Config"
+read -p "Enter the number of your choice: " config_choice
+
 
 
 if [ "$(uname)" == "Darwin" ]; then
